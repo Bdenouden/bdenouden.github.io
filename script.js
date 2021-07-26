dc749129270245a2ea68cb35981a1e8f3ba54a85
Bram <bramdenouden96@hotmail.com>
2021-07-26 17:21:13 +0200

    cleanup and more content added

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 window.onload = async function() {
-    let json = JSON.parse(await ajaxGet('/cv.json'))
-        // console.log(json);
+    // ensure picture location on reload
+    scrollFunction();
 
+    // retrieve content from json
+    let json = JSON.parse(await ajaxGet('/cv.json'))
 
     // cycle through each section (education, expercience, etc)
     for (const [section, content] of Object.entries(json)) {
-        console.log(section, content)
-
         // get table which contains the data and row template
         const tableEl = document.getElementById(`${section}-table`);
         const template = document.getElementById(`${section}-template`);
-        console.log(template)
 
         // walk through all items in the content array
         // this array contains the individual fields
@@ -18,38 +17,20 @@ window.onload = async function() {
             var itemEl = template.content.cloneNode(true);
 
             // walk across all properties to check if the item is an object or array
-
-
             walkObject(itemEl, item)
-                // for (const [key, value] of Object.entries(item)) {
-                //     // console.log(typeof item['info'] == 'object')
-                //     if (typeof value == 'string') {
-                //         insertValue(itemEl, key, value)
-                //     } else if (typeof value == 'object') {
-                //         console.log("OBJECT",key,value)
-                //     }
-                // }
-
-
-
             tableEl.appendChild(itemEl)
         });
-        // 
-        // console.log(key, entry);
     }
 
 }
 
 function walkObject(itemEl, item) {
     for (const [key, value] of Object.entries(item)) {
-        // console.log(typeof item['info'] == 'object')
         if (typeof value == 'string') {
             insertValue(itemEl, key, value)
         } else if (key == 'extra') {
             insertExtra(itemEl, value)
-            console.log("extra spotted")
         } else if (typeof value == 'object') {
-            // console.log("OBJECT", key, value)
             walkObject(itemEl, value)
         }
     }
@@ -57,47 +38,37 @@ function walkObject(itemEl, item) {
 
 
 function insertValue(itemEl, key, value) {
-    console.log(key, value)
-        // find matching classname in document and replace data
+    // find matching classname in document and replace data
     var field = itemEl.querySelectorAll(`.${key}`);
-    // console.log(typeof item.period)
     if (field.length > 0) {
         field[0].textContent = value;
-        console.log(itemEl)
     }
 }
 
 function insertExtra(itemEl, obj) {
     var field = itemEl.querySelectorAll(`.extra`);
-    console.log(field)
-    let content="";
+    let content = "";
     if (field.length > 0) {
-        for(const [key, value] of Object.entries(obj)){
-            content += `<b>${key.capitalize()}: </b><span>${value.capitalize()}</span><br>`
+        for (const [key, value] of Object.entries(obj)) {
+            content += `<span>${key.capitalize()}: </span><span>${value.capitalize()}</span><br>`
         }
-
-
         field[0].innerHTML = content;
-        console.log(field, field[0])
     }
 }
 
+// Capitalizes first letter for strings
 String.prototype.capitalize = function() {
     return this.charAt(0).toUpperCase() + this.slice(1);
 }
 
-
-
-
-// TODO check if page opens halfway (refresh after scrolling)
 // When the user scrolls down 50px from the top of the document, resize the header's font size
 window.onscroll = function() { scrollFunction() };
 
 function scrollFunction() {
-    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
         document.getElementById("img-me").classList.add("img-round", "mini-me");
         document.getElementById("header").classList.add("align-left");
-    } else if (document.body.scrollTop < 10 || document.documentElement.scrollTop < 10) {
+    } else if (document.body.scrollTop < 30 || document.documentElement.scrollTop < 30) {
         document.getElementById("img-me").classList.remove("img-round", "mini-me");
         document.getElementById("header").classList.remove("align-left");
     } else {
